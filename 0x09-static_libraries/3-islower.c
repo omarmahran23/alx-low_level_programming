80a010ad5a3c41d02f2d444ce14318722b433fa6
omarmahran23 <omar@example.com>
2023-08-04 07:39:50 -0700

    created main.h file

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,16 +1,11 @@
 #include "main.h"
-
-/**
- * _islower - check if char is lowercase
- * @c: is the char to be checked
- * Return: 1 if char is lowercase, otherwise 0.
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+    return (c >= 'a' && c <= 'z');
 }
 
